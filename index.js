00f7c9b05d57f1c900de8f95f6753c1613db1651
Sunni05 <sunni@noicegens.ml>
2021-02-10 13:55:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ client.once('ready', () => {
     console.log('READY!')
 });
 
+client.on('message', message => {
+    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        message.channel.send('Pong.')
+    }
+})
+
 client.login(process.env.TOKEN)
